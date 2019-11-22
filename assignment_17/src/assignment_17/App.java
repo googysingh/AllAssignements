eac85499babb110322df77f9f17762967ad2b8b6
Gurpreet Singh <googysingh@gmail.com>
2019-11-22 15:27:22 +0530

    Adding assingment 17-19

--- assignment_17/src/assignment_17/App.java
+++ assignment_17/src/assignment_17/App.java
@@ -4,46 +4,37 @@ import ignore.TestingUtils;
 
 public class App {
 
-
 	/**
 	 * 
-	
-	Given a string, compute recursively a new string where all the 
-		lowercase 'o' chars have been moved to the end of the string.
-
-	<br>
-	<br>
-
+	 * 
+	 * Given a string, compute recursively a new string where all the lowercase 'o'
+	 * chars have been moved to the end of the string.
+	 * 
+	 * <br>
+	 * <br>
+	 * 
 	 * <b>EXPECTATIONS:</b><br>
-		endoo("oore") <b>---></b> reoo<br>
-		endoo("oohoi") <b>---></b> hiooo <br>
-		endoo("oanotgo") <b>---></b> antgooo <br>
+	 * endoo("oore") <b>---></b> reoo<br>
+	 * endoo("oohoi") <b>---></b> hiooo <br>
+	 * endoo("oanotgo") <b>---></b> antgooo <br>
 	 */
-	
+
 	public static String endoo(String str) {
-		
-		// complete the body of this method
-		return str;
-		
-		
+		String result = "";
+		String OO = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != 'o') {
+				result = result + str.charAt(i);
+			} else {
+				OO = OO + str.charAt(i);
+			}
+		}
+		return result.concat(OO);
 	}
 
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	//----------------------STARTING POINT OF PROGRAM. IGNORE BELOW --------------------//
-	public static void main(String args[]){
+	// ----------------------STARTING POINT OF PROGRAM. IGNORE BELOW
+	// --------------------//
+	public static void main(String args[]) {
 		TestingUtils.runTests();
-		
 	}
 }
-
--- assignment_18/src/assignment_18/App.java
+++ assignment_18/src/assignment_18/App.java
@@ -4,44 +4,44 @@ import ignore.TestingUtils;
 
 public class App {
 
-
 	/**
 	 * 
-	
-	Given a string, compute recursively a new string where identical chars 
-	that are adjacent in the original string are separated from each other by a "-".
-
-	<br>
-	<br>
-
+	 * 
+	 * Given a string, compute recursively a new string where identical chars that
+	 * are adjacent in the original string are separated from each other by a "-".
+	 * 
+	 * <br>
+	 * <br>
+	 * 
 	 * <b>EXPECTATIONS:</b><br>
-		repeatHyphen("hello") <b>---></b> hel-lo<br>
-		repeatHyphen("xxyy") <b>---></b> x-xy-y <br>
-		repeatHyphen("aaaa") <b>---></b> a-a-a-a <br>
+	 * repeatHyphen("hello") <b>---></b> hel-lo<br>
+	 * repeatHyphen("xxyy") <b>---></b> x-xy-y <br>
+	 * repeatHyphen("aaaa") <b>---></b> a-a-a-a <br>
 	 */
-	
-	public static String hyphenSplit(String str) {
 
-		// complete the body of this method
-		return str;
+	public static String hyphenSplit(String str) {
+		String result = "";
+		int l = str.length();
+		if (l < 1 || l < 0) {
+			result = str;
+		} else {
+			result = String.valueOf(str.charAt(0));
+			for (int i = 1; i < l; i++) {
+				if (str.charAt(i - 1) == str.charAt(i)) {
+					result = result + "-" + str.charAt(i);
+				} else {
+					result = result + str.charAt(i);
+				}
+			}
+		}
+
+		return result;
 	}
 
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	//----------------------STARTING POINT OF PROGRAM. IGNORE BELOW --------------------//
-	public static void main(String args[]){
+	// ----------------------STARTING POINT OF PROGRAM. IGNORE BELOW
+	// --------------------//
+	public static void main(String args[]) {
 		TestingUtils.runTests();
-		
+
 	}
 }
-
